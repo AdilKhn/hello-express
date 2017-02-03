e696d511122c6a215d495e527cafb44f97324772
Adil Khan <adil.khan@outlook.com>
2017-02-02 20:49:36 -0500

    Make it Work With React On Same Machine - CORS

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ console.log("HERE");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 const du = new DataUtils();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const router = express.Router();
 
@@ -22,7 +22,25 @@ router.post('/name', function(req, res) {
 });
 
 
-app.use('/api', router);
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //
+  //         // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  //
+  //                 // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  //
+  //                         // Set to true if you need the website to include cookies in the requests sent
+  //                             // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  //
+  //                                     // Pass to next layer of middleware
+  next();
+});
+
+app.use('/api', router);
 app.listen(port);
 console.log('App is running on port' + port);
